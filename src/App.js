7b1bb99b01c33d9c4093ad02dff757d433959a0c
jason_glb <shiyujia@kean.edu>
2023-01-13 02:24:00 -0500

    route ini

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 import NavigationBar from "./Components/NavBar/NavigationBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
+import Cv from "./Components/CV/CV";
+import Research from "./Components/Research/Research";
+import Project from "./Components/Project/Project";
+import Blog from "./Components/Blog/Blog";
+import Fact from "./Components/Fact/Fact";
 const App = () => {
     return (
         <div>
-            {/*<BrowserRouter>*/}
-            {/*    <Routes>*/}
-            {/*        <Route path="/" element={<Home />} />*/}
-            {/*    </Routes>*/}
-            {/*</BrowserRouter>*/}
             <NavigationBar/>
-
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/cv" element={<Cv />} />
+                    <Route path="/research" element={<Research />} />
+                    <Route path="/project" element={<Project />} />
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="/fact" element={<Fact />} />
+                </Routes>
         </div>
     );
 };
---
+++ src/Components/Fact/Fact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Fact = () => {
+    return (
+        <div>
+            fact
+        </div>
+    );
+};
+
+export default Fact;
