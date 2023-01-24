e06172fe9bab7f84018b2e0ff8c9753c757c4580
jason_glb <shiyujia@kean.edu>
2023-01-24 00:45:56 -0500

    update about me

--- src/Components/Home/Elements/Brief/Brief.js
+++ src/Components/Home/Elements/Brief/Brief.js
@@ -12,14 +12,22 @@ const Brief = () => {
                         <small className="text-muted"></small>
                     </div>
                     <p className="mb-1" style={{textAlign:"left"}}>
-                        Hi, my name is Shiyu Jiang (江世宇), a postgraduate computer science student at Whiting School of
-                        Engineering, Johns Hopkins University. In the past, I have working experience at the Chinese
-                        Academy of Sciences advised by
-                        <a href="https://people.ucas.edu.cn/~zhenshen?language=en"> Dr.Zhen Shen</a> in algorithm development,
-                        and Wenzhou-Kean University advised by <a href="https://wku.edu.cn/faculty/aloysius-wong/"> Dr.Aloysius Wong </a>
+                        This is Shiyu Jiang (江世宇), a master student majoring in computer science at Whiting School of
+                        Engineering, Johns Hopkins University. In the past, I have working experience as a research
+                        assistant at the Chinese Academy of Sciences advised by
+                        <a href="https://people.ucas.edu.cn/~zhenshen?language=en"> Dr. Zhen Shen</a> in algorithm development,
+                        and Wenzhou-Kean University advised by <a href="https://wku.edu.cn/faculty/aloysius-wong/"> Dr. Aloysius Wong </a>
                          in Bioinformatics.
+                    </p>
+
+                    <p className="mb-1" style={{textAlign:"left"}}>
                         Currently, I am working as a research assistant in the Center for Systems Science and Engineering
-                        at JHU advised by <a href="https://systems.jhu.edu/kimia/"> Dr.Kimia Ghobadi</a> in simulation model development on the topic of infectious diseases.
+                        at JHU advised by <a href="https://systems.jhu.edu/kimia/"> Dr. Kimia Ghobadi</a> in simulation model development on the topic of infectious diseases,
+                        and working at <a href="https://zanglab.github.io/"> Chongzhi Zang Lab</a> in bioinformatics at UVa School of Medicine.
+                    </p>
+                    <p className="mb-1" style={{textAlign:"left"}}>
+                        I am planning to apply for a computer science Ph.D. degree in the fields of bioinformatics,
+                        computational biology and healthcare systems.
                     </p>
                     <small className="text-muted"></small>
                 </a>
