ce7fef5bf7f04512842334363a625229e3d9c6e5
jason_glb <shiyujia@kean.edu>
2023-01-30 18:57:42 -0500

    Update Brief.js

--- src/Components/Home/Elements/Brief/Brief.js
+++ src/Components/Home/Elements/Brief/Brief.js
@@ -21,13 +21,13 @@ const Brief = () => {
                     </p>
 
                     <p className="mb-1" style={{textAlign:"left"}}>
-                        Currently, I am working as a research assistant in the Center for Systems Science and Engineering
+                        Currently, I am working as a research assistant at Dr.Ghobadi's Lab
                         at JHU advised by <a href="https://systems.jhu.edu/kimia/"> Dr. Kimia Ghobadi</a> in simulation model development on the topic of infectious diseases,
-                        and working at <a href="https://zanglab.github.io/"> Chongzhi Zang Lab</a> in bioinformatics at UVa School of Medicine.
+                        and working at <a href="https://zanglab.github.io/"> Chongzhi Zang Lab</a> at UVa School of Medicine in bioinformatics and computational biology.
                     </p>
                     <p className="mb-1" style={{textAlign:"left"}}>
-                        I am planning to apply for a computer science Ph.D. degree in the fields of bioinformatics,
-                        computational biology and healthcare systems.
+                        {/*I am planning to apply for a computer science Ph.D. degree in the fields of bioinformatics,*/}
+                        {/*computational biology and healthcare systems.*/}
                     </p>
                     <small className="text-muted"></small>
                 </a>
@@ -37,11 +37,11 @@ const Brief = () => {
                         <small className="text-muted"></small>
                     </div>
                     <p className="mb-1" style={{textAlign:"left"}}>
-                        My research interests include bioinformatics, software engineering, and healthcare.
+                        My research interests include bioinformatics, software engineering, and healthcare systems.
                         I applied deep learning and machine learning skills in fields like spatial analysis,
                         computer vision, quantitative trading.
                         Besides, I built a bioinformatic web tool called HNOXPred by developing protein sequencing algorithms,
-                        which leads me get interested in Bioinformatics area.
+                        which leads me get interested in Bioinformatics and comp bio.
                     </p>
                     <small className="text-muted"></small>
                 </a>
@@ -63,8 +63,8 @@ const Brief = () => {
                         <small className="text-muted"></small>
                     </div>
                     <p className="mb-1" style={{textAlign:"left"}}>
-                        In my part-time, I am a soccer fan and skillfull soccer player.
-                        I was a soccer team captain of my high school and undergrad university (welcomed to invite me to the match).
+                        During my leisure time, I am a soccer fan and skillfull soccer player.
+                        I was a soccer team captain of my high school and undergrad university.
                         Besides, I can play table tennis very well and won top prize when I was young.
                         I like the competitive feeling that sports bring, which can also bring me fitness and confidence.
                         I also like gym and traveling.
